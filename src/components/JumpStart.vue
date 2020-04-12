2b0b6b64d5e4d83d717dabf6a4052cb69eecb373
Jason Ure <jason.ure@mail.weber.edu>
2020-04-11 22:32:43 -0600

    fixing

---
+++ src/components/JumpStart.vue
@@ -0,0 +1,42 @@
+<template>
+  <div >
+    <div class="d-flex justify-content-center align-items-center flex-column p-3 box">
+    <h2>Why Do Humans Dance?</h2>
+    <hr>
+    <h3>These are some basic reasons</h3>
+    <ul>
+      <li>&#8669; Personal, Family, and Cultural Aesthetics</li>
+      <li>&#8669; Freedom</li>
+      <li>&#8669; Expession</li>
+      <li>&#8669; Passion</li>
+      <li>&#8669; Love</li>
+      <li>&#8669; Respect</li>
+      <li>&#8669; Rights</li>
+      <li>&#8669; To live life at the fullest</li>
+    </ul>
+    <hr>
+  </div>
+    <small class="red"><i>Compiled explenation By: Jason Ure August 11, 2020</i></small>
+    <h3>For Experssion and Freedom</h3>
+    
+    <p>People over the decads and centry's have made great way into dance by expression
+      and freedom. For example the tribes of the native world dance was a way to show great love for thier ancestors
+      and family. This was passed down generatio to generation and still is strong in the indian culture today. Another example is
+      expresstion and freedom in cuture dance. You can really tell how people behave or act by the type dance they do, for example, 
+      african dance which which later transpired jazz dance in america. Becuase of this, I have a firm belief that it 
+      has made way of thier freedom and integrety here in america. Naturally their are alot of examples of movments in dance
+      that transform actural movements in the political world.
+    </p>
+  </div>
+    
+  
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+    
+</style>
